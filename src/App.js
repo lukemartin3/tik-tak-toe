6e631f7de3fb76cc8069a88256fbac49a73653cf
Luke Martin <lmartin9@uiowa.edu>
2023-09-29 00:33:37 -0500

    implemented jump to function

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ function calculateWinner(squares) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
@@ -84,7 +85,8 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
